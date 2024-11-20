87b6239d7cd9a45a4ae8b7545aba29558afd8152
Andrei Ionut Damian <andrei.damian@me.com>
2024-11-20 11:41:02 +0200

    fix: added `DISABLED` log

--- naeural_core/business/base/base_plugin_biz_loop.py
+++ naeural_core/business/base/base_plugin_biz_loop.py
@@ -386,8 +386,10 @@ class _BasePluginLoopMixin(object):
     return triggered  
   
   def __on_init(self):
+    if self.cfg_disabled:
+      self.P(f"WARNING: This plugin instance of `{self.__class__.__name__}` is DISABLED", boxed=True, color='r')
     if self.cfg_dataset_builder:
-      self.P(f"WARNING: Dataset builder active for {self.__class__.__name__}", boxed=True, color='r')
+      self.P(f"WARNING: Dataset builder active for `{self.__class__.__name__}`", boxed=True, color='r')      
     self._on_init()
     return
   

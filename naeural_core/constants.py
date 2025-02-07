8a3444616f1eb479af4224a54b8553cb29338da1
Cristi Bleotiu <cristibleotiu@gmail.com>
2025-02-07 18:21:00 +0200

    fix: temp reducing of SUPERVISOR_MIN_AVAIL_PRC to 0.6 for testing purposes

--- naeural_core/constants.py
+++ naeural_core/constants.py
@@ -29,7 +29,7 @@ EE_EPOCH_INTERVAL_SECONDS_KEY = 'EE_EPOCH_INTERVAL_SECONDS'
 DEFAULT_GENESYS_EPOCH_DATE = "2025-02-03 17:00:00"      # "2025-02-03 17:00:00" for mainnet
 DEFAULT_EPOCH_INTERVALS = 24                            # 24 mainnet, 1 for devnet
 DEFAULT_EPOCH_INTERVAL_SECONDS = 3600                   # 3600
-SUPERVISOR_MIN_AVAIL_PRC = 0.98                         # 0.98 for mainnet, 60% for testnet
+SUPERVISOR_MIN_AVAIL_PRC = 0.6                         # 0.98 for mainnet, 60% for testnet
 ###################################################
 ###################################################
 ###################################################

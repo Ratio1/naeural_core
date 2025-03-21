3278e9d17b993e8c6bd629d306b57a0fc1752ceb
Andrei Ionut Damian <andrei.damian@me.com>
2025-03-21 22:21:35 +0200

    fix: (HOT) added generic methods for supervisor web apps

--- naeural_core/business/default/web_app/supervisor_fast_api_web_app.py
+++ naeural_core/business/default/web_app/supervisor_fast_api_web_app.py
@@ -20,6 +20,59 @@ class SupervisorFastApiWebApp(BasePlugin):
     self.__supervisor_fastapi_plugin_running = None
     super(SupervisorFastApiWebApp, self).on_init()
     return
+  
+  def __get_current_epoch(self):
+    """
+    Get the current epoch of the node.
+
+    Returns
+    -------
+    int
+        The current epoch of the node.
+    """    
+    return self.netmon.epoch_manager.get_current_epoch()
+    
+  
+  def __sign(self, data):
+    """
+    Sign the given data using the blockchain engine.
+    Returns the signature. 
+    Use the data param as it will be modified in place.
+    """
+    signature = self.bc.sign(data, add_data=True, use_digest=True)
+    return signature
+
+  def _get_response(self, dct_data: dict):
+    """
+    
+    Create a response dictionary with the given data.
+
+    Parameters
+    ----------
+    dct_data : dict
+        The data to include in the response - data already prepared 
+
+    Returns
+    -------
+    dict
+
+    """
+    try:
+      str_utc_date = self.datetime.now(self.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+      dct_data['server_info'] = {
+        'alias': self.node_id,
+        'version': self.ee_ver,
+        'time': str_utc_date,
+        'current_epoch': self.__get_current_epoch(),
+        'uptime': str(self.timedelta(seconds=int(self.time_alive))),
+      }
+      self.__sign(dct_data) # add the signature over full data
+    except Exception as e:
+      self.P("Error in `get_response`: {}".format(e), color='r')
+      preexisting_error = dct_data.get('error', "")
+      dct_data['error'] = f"{preexisting_error} - {e}"
+    return dct_data  
+  
 
   @property
   def __is_enabled(self):

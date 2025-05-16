e148bb616b927f85e10d68c976593de53d090f30
Cristi Bleotiu <cristibleotiu@gmail.com>
2025-05-16 10:02:49 +0300

    fix(HOT): moved additional llm files to edge_node repo

--- naeural_core/serving/ai_engines/stable.py
+++ naeural_core/serving/ai_engines/stable.py
@@ -96,24 +96,6 @@ AI_ENGINES['a_dummy_cv_ai_engine'] = {
   }
 }
 
-AI_ENGINES['code_generator'] = {
-   'SERVING_PROCESS': 'code_llama_v2'
-}
-
-
-AI_ENGINES['llm'] = {
-  'SERVING_PROCESS': 'llama_v31'
-}
-
-AI_ENGINES['llm_ro'] = {
-  'SERVING_PROCESS': 'ro_llama_v31'
-}
-
-
-AI_ENGINES['doc_embed'] = {
-  'SERVING_PROCESS': 'mxbai_embed'
-}
-
 
 AI_ENGINES['th_training'] = {
   'SERVING_PROCESS': 'th_training',

a9faebe7eecbafa1dcb592928e01522b165e5a2f
Alessandro <defranceschi_a@yahoo.com>
2025-10-07 10:45:07 +0200

    feat: add get_addresses_balances bc wrapper

--- naeural_core/utils/plugins_base/bc_wrapper.py
+++ naeural_core/utils/plugins_base/bc_wrapper.py
@@ -775,6 +775,27 @@ class BCWrapper:
     Check if the last epoch has been allocated.
     """
     return self.__bc.web3_get_is_last_epoch_allocated()
+
+  def get_addresses_balances(self, addresses: list):
+    """
+    Get the ETH and $R1 balances for a list of addresses.
+
+    Parameters
+    ----------
+    addresses : list
+        The list of addresses to check the balances for.
+
+    Returns
+    -------
+    dict
+        A dictionary with the following structure:
+        {
+          "address1": {"eth": float, "r1": float},
+          "address2": {"eth": float, "r1": float},
+          ...
+        }
+    """
+    return self.__bc.web3_get_addresses_balances(addresses=addresses)
   
   
   def get_evm_net_data(self):
